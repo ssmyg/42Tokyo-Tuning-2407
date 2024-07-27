3ad886bb917b4f611e08734077bda41fb22b6aea
susumuyagi <susumu.yagi@gmail.com>
2024-07-27 13:58:51 +0900

    セッショントークン生成の効率化

--- webapp/backend/src/utils.rs
+++ webapp/backend/src/utils.rs
@@ -1,3 +1,6 @@
+use rand::distributions::Alphanumeric;
+use base64::{encode};
+
 use argon2::{
     password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
     Argon2,
@@ -6,16 +9,28 @@ use rand::Rng;
 
 use crate::errors::AppError;
 
+// pub fn generate_session_token() -> String {
+//     let mut rng = rand::thread_rng();
+//     let token: String = (0..30)
+//         .map(|_| {
+//             let idx = rng.gen_range(0..62);
+//             let chars = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//             chars[idx] as char
+//         })
+//         .collect();
+//     token
+// }
+
+/// セッショントークンを生成する関数
 pub fn generate_session_token() -> String {
-    let mut rng = rand::thread_rng();
-    let token: String = (0..30)
-        .map(|_| {
-            let idx = rng.gen_range(0..62);
-            let chars = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-            chars[idx] as char
-        })
+    // 32バイトのランダムなデータを生成
+    let random_bytes: Vec<u8> = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(32)
         .collect();
-    token
+
+    // ベース64エンコーディングを使用して文字列に変換
+    encode(random_bytes)
 }
 
 pub fn hash_password(password: &str) -> Result<String, AppError> {
