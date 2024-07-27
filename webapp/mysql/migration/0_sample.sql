4e48d7d293d6daf69eeb6421b787f279db4e096e
susumuyagi <susumu.yagi@gmail.com>
2024-07-27 22:58:58 +0900

    index追加 locations(tow_truck_id), tow_trucks(driver_id)

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -4,3 +4,7 @@ ALTER TABLE users ADD INDEX username_idx (username);
 ALTER TABLE edges ADD INDEX node_a_id_idx (node_a_id);
 ALTER TABLE edges ADD INDEX node_b_id_idx (node_b_id);
 
+ALTER TABLE tow_trucks ADD INDEX driver_id_idx (driver_id);
+ALTER TABLE locations ADD INDEX tow_truck_id_idx (tow_truck_id);
+
+
