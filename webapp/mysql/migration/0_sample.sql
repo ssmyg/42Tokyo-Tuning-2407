092ee2462d1a4af6e220b15cf154cc44fd40f9a5
susumuyagi <susumu.yagi@gmail.com>
2024-07-27 22:40:27 +0900

    edgesにindex追加

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,4 +1,6 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
 ALTER TABLE users ADD INDEX username_idx (username);
 
+ALTER TABLE edges ADD INDEX node_a_id_idx (node_a_id);
+ALTER TABLE edges ADD INDEX node_b_id_idx (node_b_id);
 
