c37ae28e4513456f0bda31be12db3b125eebc401
susumuyagi <susumu.yagi@gmail.com>
2024-07-27 16:02:34 +0900

    index修正

--- webapp/mysql/migration/0_sample.sql
+++ webapp/mysql/migration/0_sample.sql
@@ -1,5 +1,5 @@
 -- このファイルに記述されたSQLコマンドが、マイグレーション時に実行されます。
-ALTER TABLE users ADD INDEX username_idx username;
-ALTER TABLE sessions ADD INDEX user_id_idx user_id;
+ALTER TABLE users ADD INDEX username_idx (username);
+ALTER TABLE sessions ADD INDEX user_id_idx (user_id);
 
 
